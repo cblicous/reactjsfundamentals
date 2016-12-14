2218adf4032ef55af2a9c4a6c4c142fd595c4127
Christian Belka <Christian.Belka@mgm-tp.com>
2016-12-14 11:02:12 +0100

    video6

--- github-battle2/app/containers/ConfirmBattleContainer.js
+++ github-battle2/app/containers/ConfirmBattleContainer.js
@@ -1,5 +1,6 @@
 var React = require('react');
 var ConfirmBattle = require('../components/ConfirmBattle');
+var githubHelpers = require('../utils/githubHelpers');
 
 var ConfirmBattleContainer = React.createClass({
   contextTypes: {
@@ -7,13 +8,19 @@ var ConfirmBattleContainer = React.createClass({
   },
   getInitialState: function () {
     return {
-      isLoading: false,
+      isLoading: true,
       playersInfo: [],
     }
   },
   componentDidMount: function () {
     var query = this.props.location.query;
-    // Fetch info from github then update state
+    githubHelpers.getPlayersInfo([query.playerOne, query.playerTwo])
+      .then(function (players) {
+        this.setState({
+          isLoading: false,
+          playersInfo: [players[0], players[1]]
+        })
+      }.bind(this))
   },
   render: function () {
     return (
@@ -24,4 +31,4 @@ var ConfirmBattleContainer = React.createClass({
   }
 });
 
-module.exports = ConfirmBattleContainer;
+module.exports = ConfirmBattleContainer
---
+++ github-battle2/app/utils/githubHelpers.js
@@ -0,0 +1,32 @@
+var axios = require('axios');
+
+var id = "YOUR_CLIENT_ID";
+var sec = "YOUR_SECRET_ID";
+var param = "?client_id=" + id + "&client_secret=" + sec;
+
+function getUserInfo (username) {
+  return axios.get('https://api.github.com/users/' + username + param);
+}
+
+var helpers = {
+  getPlayersInfo: function (players) {
+    // axios.all calls each axios.get
+    return axios.all(
+      players.map(function (username) {
+      return getUserInfo(username)
+      })
+    )
+    .then(function (info) {
+
+    // he uses map to create the new array with just the data on the items
+      return info.map(function (user) {
+        return user.data
+      })
+    })
+    .catch(function (err) {
+      console.warn('Error in getPlayersInfo: ', err)
+    })
+  }
+};
+
+module.exports = helpers;
